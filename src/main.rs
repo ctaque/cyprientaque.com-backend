b0a65c0cb67b136268c8904d689d3b7ca9cb9284
Cyprien Taque <cyprien.taque@gmail.com>
2020-06-01 12:26:54 +0200

    Ajout de la connexion dans l'appstate et d'un filter sur les deleted

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,26 @@
 extern crate ctprods;
 extern crate diesel;
 
-use actix_web::{get, web, App, HttpServer, HttpResponse };
+use actix_web::{ get, web, App, HttpServer, HttpResponse };
 use serde_json::json;
 use self::ctprods::establish_connection;
 use self::diesel::prelude::*;
 use diesel::result::Error;
+use diesel::pg::PgConnection;
 use self::ctprods::models::Project;
+
+struct AppState {
+    db: PgConnection
+}
 
 #[derive(serde::Deserialize)]
 struct GetProjectInfo{
     id: i32,
 }
 #[get("/projects/{id}")]
-async fn index(info: web::Path<GetProjectInfo>) -> Result<HttpResponse, HttpResponse> {
-    use ctprods::schema::projects::dsl::projects;
-    let connection = establish_connection();
-    let result: Result<Project, Error> = projects.find::<i32>(info.id.into()).first(&connection);
+async fn index(data: web::Data<AppState>, info: web::Path<GetProjectInfo>) -> Result<HttpResponse, HttpResponse> {
+    use ctprods::schema::projects::dsl::{ projects, deleted_at };
+    let result: Result<Project, Error> = projects.filter(deleted_at.is_null()).find::<i32>(info.id.into()).first(&data.db);
 
     match result {
         Ok(project) => Ok(HttpResponse::Ok().body(json!(project))),
@@ -30,7 +34,7 @@ async fn index(info: web::Path<GetProjectInfo>) -> Result<HttpResponse, HttpResp
 #[actix_rt::main]
 async fn main() -> std::io::Result<()> {
     HttpServer::new(
-        || App::new().service(index)
+        || App::new().data(AppState{db: establish_connection()}).service(index)
     ).bind("127.0.0.1:8088")?
         .run()
         .await
