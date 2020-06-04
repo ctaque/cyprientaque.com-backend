8ec3513770f67421e597d1a8dc87642fdc5307bf
Cyprien Taque <cyprien.taque@gmail.com>
2020-06-04 19:46:56 +0200

    Ajout de la catégories aux projets

--- src/models/project.rs
+++ src/models/project.rs
@@ -1,6 +1,6 @@
 use chrono::naive::NaiveDateTime;
 use async_trait::async_trait;
-use crate::models::{ model::{ Model, NewModel } };
+use crate::models::{ ProjectCategory, model::{ Model, NewModel } };
 use postgres::{ Row, error::Error };
 
 
@@ -17,7 +17,8 @@ pub struct Project {
     pub created_at: Option<NaiveDateTime>,
     pub updated_at: Option<NaiveDateTime>,
     pub sketchfab_model_number: Option<String>,
-    pub user_id: i32
+    pub user_id: i32,
+    pub category: Option<ProjectCategory>
 }
 
 #[derive(Clone, serde::Serialize, serde::Deserialize)]
@@ -34,15 +35,19 @@ pub struct NewProject {
 impl Model<Project> for Project {
     async fn find(project_id: i32) -> Result<Project, Error>
     where Project: 'async_trait{
-        let row: Row = Self::db().await.query_one("select * from projects where id = $1 and deleted_at = null;",  &[&project_id]).await?;
-        Ok(Project::new(row))
+        let row: Row = Self::db().await.query_one("select * from projects where id = $1 and deleted_at is null;",  &[&project_id]).await?;
+        let p = Project::new(&row);
+        let p = p.attach_category().await;
+        p
     }
     async fn all() -> Result<Vec<Project>, Error>
         where Project: 'async_trait{
-        let rows: Vec<Row> = Self::db().await.query("select * from projects;", &[]).await?;
+        let rows: Vec<Row> = Self::db().await.query("select * from projects where deleted_at is null;", &[]).await?;
         let mut projects = Vec::new();
         for row in rows{
-            projects.push(Project::new(row));
+            let p = Project::new(&row);
+            let p = p.attach_category().await?;
+            projects.push(p);
         }
         Ok(projects)
     }
@@ -50,18 +55,21 @@ impl Model<Project> for Project {
 
         let row: Row = Self::db().await.query_one("update projects set category_id = $2, title = $3, slug = $4, content = $5, views_count = $6, likes_count = $7, deleted_at = $8, created_at = $9, updated_at = CURRENT_TIMESTAMP, sketchfab_model_number = $10, user_id = $11 where id = $1 returning *;",
                                     &[&self.id, &self.category_id, &self.title, &self.slug, &self.content, &self.views_count, &self.likes_count, &self.deleted_at, &self.created_at, &self.sketchfab_model_number, &self.user_id]).await?;
-        Ok(Project::new(row))
+        let p = Project::new(&row);
+        let p = p.attach_category().await?;
+        Ok(p)
 
     }
     async fn delete(mut self) -> Result<Project, Error>{
-        self.deleted_at = Option::Some(chrono::offset::Local::now().naive_local());
-        let row = Self::db().await.query_one("update projects set deleted_at = $2 where id = $1", &[&self.id, &self.deleted_at]).await?;
-        Ok(Project::new(row))
+        let row = Self::db().await.query_one("update projects set deleted_at = CURRENT_TIMESTAMP where id = $1", &[&self.id]).await?;
+        let p = Project::new(&row);
+        let p = p.attach_category().await?;
+        Ok(p)
     }
 }
 
 impl Project{
-    pub fn new(row: Row) -> Project{
+    pub fn new<'a>(row: &Row) -> Project{
         Project {
             id: row.get("id"),
             category_id: row.get("category_id"),
@@ -74,16 +82,25 @@ impl Project{
             created_at: row.get("created_at"),
             updated_at: row.get("updated_at"),
             sketchfab_model_number: row.get("sketchfab_model_number"),
-            user_id: row.get("user_id")
+            user_id: row.get("user_id"),
+            category: None
         }
     }
     pub async fn by_category(cat_id: i32) -> Result<Vec<Project>, Error>{
         let rows: Vec<Row> = Self::db().await.query("select * from projects where category_id = $1;", &[&cat_id]).await?;
         let mut projects = Vec::new();
         for row in rows{
-            projects.push(Project::new(row));
+            let project = Project::new(&row);
+            let project = project.attach_category().await?;
+            projects.push(project);
         }
         Ok(projects)
+    }
+    pub async fn attach_category(mut self) -> Result<Project, Error>{
+        let row = Self::db().await.query_one("select * from project_categories where id = $1", &[&self.category_id]).await?;
+        let cat = ProjectCategory::new(&row);
+        self.category = Some(cat);
+        Ok(self)
     }
 }
 
@@ -94,7 +111,10 @@ impl NewModel<Project> for NewProject {
 
         let row: Row = Self::db().await.query_one("insert into projects (category_id, title, slug, content, created_at, sketchfab_model_number, user_id) values ($1, $2, $3, $4, CURRENT_TIMESTAMP, $6, $7);",
                                     &[&self.category_id, &self.title, &self.slug, &self.content, &self.sketchfab_model_number, &self.user_id]).await?;
-        Ok(Project::new(row))
+
+        let project = Project::new(&row);
+        let project = project.attach_category().await?;
+        Ok(project)
     }
 }
 
--- src/models/project_category.rs
+++ src/models/project_category.rs
@@ -1,8 +1,20 @@
+use postgres::{ Row };
 
-#[derive(diesel::Queryable, serde::Serialize, serde::Deserialize)]
+#[derive(diesel::Queryable, Clone, Debug, serde::Serialize, serde::Deserialize)]
 pub struct ProjectCategory {
     pub id: i32,
     pub name: String,
     pub picture_url: Option<String>,
     pub slug: String,
+}
+
+impl ProjectCategory{
+    pub fn new(row: &Row) -> Self{
+        ProjectCategory{
+            id: row.get("id"),
+            name: row.get("name"),
+            picture_url: row.get("picture_url"),
+            slug: row.get("slug")
+        }
+    }
 }
