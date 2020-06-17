93359084a7ca6f9af58729c601ab95f5de2026e3
Cyprien Taque <cyprien.taque@gmail.com>
2020-06-17 20:44:59 +0200

    ajout des timestamps de project_categories au schema rust

--- src/schema.rs
+++ src/schema.rs
@@ -1,9 +1,69 @@
 table! {
+    comments (id) {
+        id -> Int4,
+        content -> Text,
+        user_id -> Nullable<Int4>,
+        project_id -> Int4,
+        created_at -> Nullable<Timestamp>,
+        updated_at -> Nullable<Timestamp>,
+        deleted_at -> Nullable<Timestamp>,
+    }
+}
+
+table! {
+    migrations (id) {
+        id -> Int4,
+        migration -> Varchar,
+        batch -> Int4,
+    }
+}
+
+table! {
+    profile_cover_images (id) {
+        id -> Int4,
+        created_at -> Nullable<Timestamp>,
+        updated_at -> Nullable<Timestamp>,
+        user_id -> Int4,
+        w3200_keyname -> Varchar,
+        w3200_object_url -> Varchar,
+        w300_keyname -> Varchar,
+        w300_object_url -> Varchar,
+    }
+}
+
+table! {
+    profile_user_images (id) {
+        id -> Int4,
+        created_at -> Nullable<Timestamp>,
+        updated_at -> Nullable<Timestamp>,
+        user_id -> Int4,
+        w1500_keyname -> Varchar,
+        w200_keyname -> Varchar,
+        w40_keyname -> Varchar,
+        w1500_object_url -> Varchar,
+        w200_object_url -> Varchar,
+        w40_object_url -> Varchar,
+    }
+}
+
+table! {
     project_categories (id) {
         id -> Int4,
         name -> Varchar,
         picture_url -> Nullable<Varchar>,
-        slug -> Varchar,
+        slug -> Nullable<Varchar>,
+        created_at -> Timestamp,
+        updated_at -> Timestamp,
+        deleted_at -> Nullable<Timestamp>,
+    }
+}
+
+table! {
+    project_image_categories (id) {
+        id -> Int4,
+        name -> Varchar,
+        created_at -> Nullable<Timestamp>,
+        updated_at -> Nullable<Timestamp>,
     }
 }
 
@@ -12,6 +72,7 @@ table! {
         id -> Int4,
         w1500_keyname -> Varchar,
         w350_keyname -> Varchar,
+        project_image_category_id -> Int4,
         w1500_object_url -> Varchar,
         w350_object_url -> Varchar,
         primary -> Bool,
@@ -25,16 +86,27 @@ table! {
     projects (id) {
         id -> Int4,
         category_id -> Int4,
+        user_id -> Int4,
         title -> Varchar,
         slug -> Varchar,
         content -> Text,
+        published -> Bool,
         views_count -> Int4,
         likes_count -> Int4,
         deleted_at -> Nullable<Timestamp>,
         created_at -> Nullable<Timestamp>,
         updated_at -> Nullable<Timestamp>,
         sketchfab_model_number -> Nullable<Varchar>,
+    }
+}
+
+table! {
+    subscriptions (id) {
+        id -> Int4,
+        subscriber_id -> Int4,
         user_id -> Int4,
+        created_at -> Nullable<Timestamp>,
+        updated_at -> Nullable<Timestamp>,
     }
 }
 
@@ -49,6 +121,7 @@ table! {
         website_url -> Nullable<Text>,
         admin -> Bool,
         active -> Bool,
+        isNewProjectEmailSubscriber -> Bool,
         deleted_at -> Nullable<Timestamp>,
         remember_token -> Nullable<Varchar>,
         created_at -> Nullable<Timestamp>,
@@ -57,13 +130,20 @@ table! {
     }
 }
 
+joinable!(comments -> projects (project_id));
+joinable!(project_images -> project_image_categories (project_image_category_id));
 joinable!(project_images -> projects (project_id));
 joinable!(projects -> project_categories (category_id));
-joinable!(projects -> users (user_id));
 
 allow_tables_to_appear_in_same_query!(
+    comments,
+    migrations,
+    profile_cover_images,
+    profile_user_images,
     project_categories,
+    project_image_categories,
     project_images,
     projects,
+    subscriptions,
     users,
 );
