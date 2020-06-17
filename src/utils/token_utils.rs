96a5b6c5be47a4d4db6a4d639a175c7e2c87ef76
Cyprien Taque <cyprien.taque@gmail.com>
2020-06-17 20:54:32 +0200

    secret vient de from_secret de .env

--- src/utils/token_utils.rs
+++ src/utils/token_utils.rs
@@ -4,7 +4,7 @@ use std::env;
 use jsonwebtoken::{DecodingKey, TokenData, Validation};
 pub fn decode_token(token: String) -> jsonwebtoken::errors::Result<TokenData<JWTToken>> {
     let secret: String = env::var("JWT_SECRET").expect("Missing JWT_SECRET in .env");
-    jsonwebtoken::decode::<JWTToken>(&token, &DecodingKey::from_base64_secret(&secret).expect("Failed Creating encoding key from base 64"), &Validation::default())
+    jsonwebtoken::decode::<JWTToken>(&token, &DecodingKey::from_secret(&secret.as_ref()), &Validation::default())
 }
 
 pub fn verify_token(token: String) -> Result<String, String> {
