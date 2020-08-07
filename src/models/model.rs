698b533c3fa40d8844aec45da55cbf52b2d3179a
Cyprien Taque <cyprien.taque@gmail.com>
2020-08-07 15:47:17 +0200

    Ajout du trait Updatable Model

--- src/models/model.rs
+++ src/models/model.rs
@@ -39,3 +39,20 @@ pub trait NewModel<T> {
     }
     async fn save(self: Self) -> Result<T, Error>;
 }
+
+#[async_trait]
+pub trait UpdatableModel<T> {
+    async fn db() -> Client
+    where T: 'async_trait{
+        let (client, connection) =
+            tokio_postgres::connect(&env::var("DATABASE_URL").expect("DATABASE_URL must be set"), NoTls).await.expect(&format!("Error connecting to {}", env::var("DATABASE_URL").unwrap()));
+
+        tokio::spawn(async move {
+            if let Err(e) = connection.await {
+                eprintln!("connection error: {}", e);
+            }
+        });
+        client
+    }
+    async fn update(self: Self) -> Result<T, Error>;
+}
