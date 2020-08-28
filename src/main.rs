b359fafb7f79adbd260f43c1c22a8e6852cb8d6e
Cyprien Taque <cyprien.taque@gmail.com>
2020-08-28 20:39:43 +0200

    remplacement de l'origine par * en cors

--- src/main.rs
+++ src/main.rs
@@ -253,8 +253,7 @@ async fn main() -> std::io::Result<()> {
                 .wrap(auth_middleware::Authentication)
                 .wrap(
                     Cors::new() // <- Construct CORS middleware builder
-                        .allowed_origin("http://localhost:3000")
-                        .allowed_origin("https://www.cyprientaque.com")
+                        .allowed_origin("*")
                         .allowed_methods(vec!["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"])
                         .allowed_headers(vec![http::header::AUTHORIZATION, http::header::ACCEPT])
                         .allowed_header(http::header::CONTENT_TYPE)
