1cb1a950726428fd829b68371a6507be5450bac1
Cyprien Taque <Cyprien.taque@gmail.com>
2023-01-21 18:18:41 +0100

    suppression helmet

--- static/react/src/App.tsx
+++ static/react/src/App.tsx
@@ -1,14 +1,9 @@
 import * as React from 'react';
-import { Helmet } from 'react-helmet';
 import Home from './Components/Home';
 
 const App = () => {
     return (
         <div className="AppWrapper" id="outer-container" >
-            <Helmet>
-                <title>Cv de Cyprien Taque </title>
-                <meta name="description" content="Cv interactif de Cyprien Taque. Ici vous trouverez de l'informatique, de la CAO et du travail du bois" />
-            </Helmet>
             <main id="pageWrap">
                 <div className="PageWrap">
                     <Home />
